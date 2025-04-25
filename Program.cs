5486d7fd1ed01a363675d50dd98eaa43668d1f4a
Raviteja <ravimahiind@gmail.com>
2025-04-25 16:05:33 -0400

    initial code changes for 3 level cascading components

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using HabitusBlazor;
 using MudBlazor.Services;
+using HabitusBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -10,4 +11,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddMudServices();
+
+builder.Services.AddSingleton<StateService>();
+
 await builder.Build().RunAsync();
