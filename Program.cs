9151d2a9888a016ad45f329abf890fc85fd392ee
Raviteja <ravimahiind@gmail.com>
2025-04-26 17:57:11 -0400

    removed libraries that were referring to stateservice

--- Program.cs
+++ Program.cs
@@ -2,7 +2,6 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using HabitusBlazor;
 using MudBlazor.Services;
-using HabitusBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -11,7 +10,5 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddMudServices();
-
-builder.Services.AddSingleton<StateService>();
 
 await builder.Build().RunAsync();
